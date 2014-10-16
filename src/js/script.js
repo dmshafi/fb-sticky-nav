e58a1ad70ab509038c529726d3c6aefd5274bf18
Dewan Ibtesham <rajputro@gmail.com>
2014-10-16 02:02:47 -0600

    trying to move features from javascript to css only. The reason behind that is - Facebook links doesn't work like regular html links, instead they populate content by js. So content scripts are not running when navigating back from user page to homepage. Another option to explore is to use background pages

--- src/js/script.js
+++ src/js/script.js
@@ -9,8 +9,17 @@ var overflow_x = "overflow-x: hidden !important ;";
 var css_height = "height:"+ height + "px !important;";
 var css_code = "#leftCol{" +overflow_x + overflow_y + 
   css_height +"}";
-var scrollbar = "::-webkit-scrollbar {width: 0px;}"
 var style = document.createElement('style');
 style.type = 'text/css';
-style.innerHTML = css_code+scrollbar;
+style.innerHTML = css_code;
 document.getElementsByTagName("head")[0].appendChild( style );
+
+var leftColumn=document.getElementById("leftCol");
+
+var pinImage = document.createElement("img");
+pinImage.setAttribute('id','pinButton')
+pinImage.setAttribute('src', 'http://cdn.flaticon.com/png/256/1658.png');
+pinImage.setAttribute('title','unpin')
+pinImage.setAttribute('alt', 'unpin');
+pinImage.setAttribute('width', '20px');
+// leftColumn.insertBefore(pinImage,leftColumn.childNodes[0]);
